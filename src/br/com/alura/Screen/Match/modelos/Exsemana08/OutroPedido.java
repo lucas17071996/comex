c6b8632c9635c7c9194b2f74f53321c522677a50
Lucas Soares <156090735+lucas17071996@users.noreply.github.com>
2024-02-15 20:21:55 -0300

    Delete src/br/com/alura/Screen/Match/modelos/Exsemana08 directory

--- src/br/com/alura/Screen/Match/modelos/Exsemana08/OutroPedido.java
+++
@@ -1,8 +0,0 @@
-package br.com.alura.Screen.Match.modelos.Exsemana08;
-
-public class OutroPedido {
-    private int id;
-    private Cliente cliente;
-    private float preco;
-    private int quantidade;
-}
